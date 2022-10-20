1d3e56e31409e9845d8fb5730a090997e2142bb6
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2022-10-20 00:06:01 -0300

    Automatização do deployer do storybook via pages

--- frontend/.storybook/main.cjs
+++ frontend/.storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/BetBall/'
+    }
+
+    return config
   }
 }
