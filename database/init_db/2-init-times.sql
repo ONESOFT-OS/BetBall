0ad0cd658a2a89071d499e32684504b61c5160a4
João Vitor Cardoso <jcardosodesign@gmail.com>
2022-10-24 23:15:37 -0300

    edit: preenchimento dos times no banco

--- database/init_db/2-init-times.sql
+++ database/init_db/2-init-times.sql
@@ -6,5 +6,26 @@ INSERT INTO times (nome, id_time, brasao) VALUES
 INSERT INTO times (nome, id_time, brasao) VALUES
     ('Flamengo', '1' ,'src assets brasoes flamengo svg'),
     ('Corinthians', '2' ,'src assets brasoes corithians svg'),
-    ('São Paulo', '3' ,'src assets brasoes saopaulo svg');
+    ('São Paulo', '3' ,'src assets brasoes saopaulo svg')
+    ('Palmeiras', '4' ,'src assets brasoes palmeiras svg'),
+    ('Internacional', '5' ,'src assets brasoes internacional svg'),
+    ('Fluminense', '6' ,'src assets brasoes fluminense svg'),
+    ('Athletico-PR', '7' ,'src assets brasoes at athletico-pr svg'),
+    ('Atletico-MG', '8' ,'src assets brasoes atletico-mg svg'),
+    ('America-MG', '9' ,'src assets brasoes america-mg svg'),
+    ('Fortaleza', '10' ,'src assets brasoes fortaleza svg'),
+    ('Botafogo', '11' ,'src assets brasoes botafogo svg'),
+    ('Santos', '12' ,'src assets brasoes santos svg'),
+    ('Bragantino', '13' ,'src assets brasoes bragantino svg'),
+    ('Goias', '14' ,'src assets brasoes goias svg'),
+    ('Coritiba', '15' ,'src assets brasoes coritiba svg'),
+    ('Ceará', '16' ,'src assets brasoes ceara svg'),
+    ('Cuiabá', '17' ,'src assets brasoes cuiaba svg'),
+    ('Avai', '18' ,'src assets brasoes avai svg'),
+    ('Atletico-GO', '19' ,'src assets brasoes atletico-go svg'),
+    ('Juventude', '20' ,'src assets brasoes juventude svg');
+
+
+
+
 
