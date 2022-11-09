c9ece786fff9588737f0f719521638b356ddb2a3
Ariel Carvalho <asncarvalho@ecomp.uefs.br>
2022-11-09 16:09:05 -0300

    adding apostas queries

--- backend/api/queries/queryAposta.py
+++ backend/api/queries/queryAposta.py
@@ -36,3 +36,34 @@ def add_aposta(nickname, valor, time):
     """
     executeQuery(connection, query)
 
+def create(nickname, valor, time):
+    query = f"""
+        INSERT INTO apostas  VALUES
+        ('{nickname}', '{valor}','{time}');
+        """
+    executeQuery(connection, query)
+
+def find_by_id(_id):
+    query = f"""
+    SELECT * FROM apostas WHERE id = '{_id}'
+    """
+    executeQuery(connection, query)
+
+def update(_id, nickname, valor, time):
+    query = f"""
+    UPDATE apostas SET 
+    (
+        nickname = '{nickname}',
+        valor = '{valor}',
+        time = '{time}',
+    )
+    WHERE ID =  '{_id}'
+    """
+
+    executeQuery(connection, query)
+
+def delete(self, _id):
+    query = f""" DELETE FROM apostas WHERE id = '{_id}' """
+
+    executeQuery(connection, query)
+
