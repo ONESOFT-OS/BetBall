56c40ad9271c81b298601574c08a224a4029a8bf
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2022-11-12 00:47:14 -0300

    edit: comentário de erro e validação do cadastro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import {RoutesBase} from './routes/RoutesBase';
 import './styles/global.css';
 
-function App(){
+export function App(){
   return (
-    <div className='App'><RoutesBase/></div>
+    <RoutesBase/>
   )
 }
 
-export default App
-function then(arg0: { new(body?: BodyInit | null | undefined, init?: ResponseInit | undefined): Response; prototype: Response; error(): Response; redirect(url: string | URL, status?: number | undefined): Response; }): string {
-  throw new Error('Function not implemented.');
-}
+// export default App
+// function then(arg0: { new(body?: BodyInit | null | undefined, init?: ResponseInit | undefined): Response; prototype: Response; error(): Response; redirect(url: string | URL, status?: number | undefined): Response; }): string {
+//   throw new Error('Function not implemented.');
+// }
 
--- frontend/src/routes/RoutesBase.tsx
+++ frontend/src/routes/RoutesBase.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes, BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ForgotPassword } from '../pages/ForgotPassword';
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
@@ -6,13 +6,13 @@ import { SignUp } from "../pages/SignUp";
 
 export const RoutesBase = () => {
    return(
-    
+    <BrowserRouter>
         <Routes>
             <Route path="/" element={<SignIn/>} />
             <Route path="/signup" element={<SignUp/>} />
             <Route path="/forgotpassword" element={<ForgotPassword />} />
         </Routes>
-    
+     </BrowserRouter>
    )
 }
 
