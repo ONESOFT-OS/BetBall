fa19a030973c2a897ba2a6404c1bf7ea39b4f402
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-11-12 02:24:48 -0300

    Add: Adicionando CORS

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 
 from model.models import Login, User
 
@@ -11,6 +12,21 @@ app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def fake_decode_token(token):
     return User(
