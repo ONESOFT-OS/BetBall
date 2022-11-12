042ac8c096517e67f4e5fed0ea24545618ec916b
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-11-12 02:59:42 -0300

    Style: adicionando respiro entre importações

--- backend/api/main.py
+++ backend/api/main.py
@@ -6,6 +6,7 @@ from model.models import Login, User, Cadastro
 
 from queries.queries import get_clubs
 from queries.users import get_users, get_users_by_type, register_user
+
 from queries.utils.service import login_user
 
 app = FastAPI()
