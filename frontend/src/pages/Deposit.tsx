8155940280396e897d5ec90c53e600249a4e73d1
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2022-11-16 01:29:00 -0300

    add: tela de deposito de crédito

--- frontend/src/pages/Deposit.tsx
+++ frontend/src/pages/Deposit.tsx
@@ -1,12 +1,25 @@
+import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
 import { Heading } from "../components/Heading";
+import { Text } from "../components/Text";
 
 export function Deposit(){
     return(
-        <div  className=" w-screen h-screen bg-[url('../assets/Gradient.svg')] bg-black">
-            <div className="flex flex-row justify-between pt-7 px-4">
-                <Heading size="xlg"> Carteira</Heading>
-                <Button mode="closer">X</Button>
+        <div  className="bg-[url('../assets/Gradient.svg')] bg-gray-900 min-w-screen min-h-screen  max-h-full max-w-full flex items-center justify-center">
+                <div className="bg-black w-[1000px] h-[500px] min-h-max min-w-max flex flex-col rounded-2xl opacity-70">
+                <header className="flex flex-row justify-between pt-12 px-10">
+                    <Heading size="xlg"> Carteira</Heading>
+                    <Button mode="closer" className="px-4">X</Button>
+                </header>
+
+                <div className="flex flex-row pt-2 px-12 space-x-8">
+                    <Text size="lg"   className="text-green-500 underline font-bold " ><Link to={'/'}>Adicionar</Link></Text>
+                    <Text size="lg"   className="font-semibold hover:text-green-500" ><Link to={'/'}>Sacar</Link></Text>
+                </div>
+
+                <div>
+                    
+                </div>
             </div>
         </div>
     )
