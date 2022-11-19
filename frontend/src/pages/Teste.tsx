d1cd7df072c20307fb2e4eb1c41c7553e0b7d770
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-11-19 14:49:43 -0300

    Delete: Apagando pagina de teste

--- frontend/src/pages/Teste.tsx
+++
@@ -1,9 +0,0 @@
-import { TeamImg } from "../components/TeamImg";
-
-export function Teste(){
-    return(
-        <div className="flex flex-row justify-center max-w-[100%] h-[100vh] bg-black">
-            <TeamImg/>
-        </div>
-    )
-}
