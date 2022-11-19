729298517ed1d56f74185d0677eb9eb5d46fad9e
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2022-11-18 21:57:46 -0300

    edit: correções NavBar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,19 @@
-import {Logo} from "../assets/Logo"
+import { Link } from "react-router-dom";
+import {Logo} from "../assets/Logo";
 import { Button } from "./Button";
+import {Text} from './Text';
 
 
 export function NavBar(){
     return(
     
-       <nav className="flex justify-between items-center w-screen h-[5rem] flex pl-20 pr-20"> 
+       <nav className="flex justify-between items-center w-screen h-[5rem] pl-20 pr-20"> 
                <Logo/>
               <ul className=" w-[20rem] flex flex-row justify-between">
-                <li><a href="http://" target="_blank">Home</a> </li> 
-                <li><a href="http://" target="_blank">Ranking</a></li> 
-                <li><a href="http://" target="_blank">Jogos</a></li>
-                <li><a href="http://" target="_blank">Sobre</a></li>
+                <li><Text className="text-white hover:text-green-500 font-semibold" size="lg"><Link to={'/'}>Home</Link></Text></li>
+                <li><Text className="text-white hover:text-green-500 font-semibold" size="lg"><Link to={'/'}>Ranking</Link></Text></li>
+                <li><Text className="text-white hover:text-green-500 font-semibold" size="lg"><Link to={'/'}>Jogos</Link></Text></li>
+                <li><Text className="text-white hover:text-green-500 font-semibold" size="lg"><Link to={'/'}>Sobre</Link></Text></li>
               </ul>
               <Button type='submit' className='w-fit m bg-transparent ring-2 ring-green-500'>Logout</Button>
        </nav>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { NavBar } from "../components/NavBar";
+import { NavBar } from "../components/Navbar";
 
 export function Home(){
     return(
