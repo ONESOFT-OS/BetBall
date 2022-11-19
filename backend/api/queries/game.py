53404fbb844f09dcd3c35bdac0c82eb21f2cebd6
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-11-19 13:46:37 -0300

    (WIP) Add: Recuperação de uma partida de jogo cadastrado, queries/game

--- backend/api/queries/game.py
+++ backend/api/queries/game.py
@@ -1,5 +1,5 @@
 from queries.core.db import createDBConnection, executeQuery, executeSelection, connection
-from queries.utils.service import format_date
+from queries.utils.service import format_date, tutple_to_dict
 
 
 # @param game_id, nickname do colaborador, data de inicio do jogo, data final, status do jogo
@@ -41,9 +41,36 @@ def add_game(collaborator_nick, end_date, id_team1, id_team2, isDone=False):
     """
     executeQuery(connection, queryNewParticipation)
 
+{
+    "game_id":"",
+    "out_team": 
+        {
+            "name": "",
+            "current_goals": 0,
+            "image_src": ""
+        },
+    "home_team":
+        {
+            "name": "",
+            "current_goals": 0,
+            "image_src": ""
+        }
+}
 
+# Retorna as informações de um jogo cadastrado
+# @return Dicionário. Id do jogo, times jogando, placar atual.
+def get_poster_by_id(game_id):
+    query = f"""
+    SELECT * FROM participacao WHERE id_jogo = '{ game_id }'
+    """
+    partidas = executeSelection(connection, query)
+    partida_dict = [tutple_to_dict('team_id', 'game_id', 'goals', tupla=partida) for partida in partidas]
+    
 
-
+# Retorna uma lista com todos os jogos cadastrados.
+# @return Lista de dicionário. Id do jogo, times jogando, placar atual.
+def get_games():
+    pass
 
 def list_game_by_id(game_id):
     query = f"""
