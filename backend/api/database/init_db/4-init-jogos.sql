031bbda463d0ea2d1f632fd31dd4a22b9b5660ea
Paulo Queiroz <paulocarvalho.pq@gmail.com>
2022-11-24 11:03:24 -0300

    fix: init jogos enviando dado incopativel com tab.

--- backend/api/database/init_db/4-init-jogos.sql
+++ backend/api/database/init_db/4-init-jogos.sql
@@ -1,5 +1,5 @@
 use `BetBall`;
 SET character_set_client = utf8;
 INSERT INTO jogos (id_jogo, nick_colaborador, data_fim_aposta, isDone) VALUES
-    ('0','MASTER','2022-10-22 12:00:00[.000]', 1),
-    ('1','MASTER','2022-10-22 15:30:00[.000]', 1);
+    ('0','MASTER','2022-10-22 12:00:00', 1),
+    ('1','MASTER','2022-10-22 15:30:00', 1);
