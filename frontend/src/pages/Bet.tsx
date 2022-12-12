61331248504a03d1fa49c06d94bd39d80510a380
Lyrton Marcell <lymarcell@gmail.com>
2022-12-12 16:44:14 -0300

    edit:adição dos componentes de saída,entrada,total

--- frontend/src/pages/Bet.tsx
+++ frontend/src/pages/Bet.tsx
@@ -24,6 +24,7 @@ import {
   } from "@chakra-ui/react";
   import { useEffect} from "react";
   import ModalComp from "../components/ModalComp";
+import Resume from "../components/Resumo";
 
 export function Bet() {
     const [busca, setBusca] = useState('');
@@ -34,6 +35,54 @@ export function Bet() {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [data, setData] = useState([]);
     const [dataEdit, setDataEdit] = useState({});
+    const data2 = localStorage.getItem("financeiro");
+    const [financeiroList, setfinanceiroList] = useState(
+      data2 ? JSON.parse(data2!) : []
+    );
+    const [income, setIncome] = useState(0);
+    const [expense, setExpense] = useState(0);
+    const [total, setTotal] = useState(0);
+  
+    useEffect(() => {
+  
+      const amountExpense = financeiroList
+        .filter((item: any) => item.expense)
+        .map((financeiro: any) => Number(financeiro.amount));
+  
+      const amountIncome = financeiroList
+        .filter((item: any) => item.expense)
+        .map((financeiro: any) => Number(financeiro.amount));
+  
+  
+      function Contador(){
+        var soma=0
+        var contador=0
+        var lista = [JSON.parse(localStorage.getItem("aposta_cliente")!).map((c: any) => c.valor)]
+        for (var i = 0; i < lista[0].length; i++){
+          soma+=Number(lista[0][i])
+          if (i==lista[0].length-1){
+            return soma
+          }
+        }
+        
+      }
+  
+      const expense = amountExpense.reduce((acc: any, cur: any) => acc + cur, Contador()).toFixed(2);
+      const income = amountIncome.reduce((acc: any, cur: any) => acc + cur, Number(["100"])).toFixed(2);
+  
+      const total: any = Math.abs(income - expense).toFixed(2);
+      setIncome(income);
+      setExpense(expense);
+      setTotal(total);
+    }, [financeiroList]);
+  
+    const handleAdd = (financeiro: any) => {
+      const newArrayFinanceiro = [...financeiroList, financeiro];
+  
+      setfinanceiroList(newArrayFinanceiro);
+  
+      localStorage.setItem("Finaceiro", JSON.stringify(newArrayFinanceiro));
+    };
 
 
     const hendleTeamUpdate = (event : any) => {
@@ -64,6 +113,7 @@ export function Bet() {
         setData(newArray);
     
         localStorage.setItem("aposta_cliente", JSON.stringify(newArray));
+        window.location.href = window.location.href;
       };
 
 
@@ -72,80 +122,87 @@ export function Bet() {
         <div className="bg-black max-w-full flex flex-col items-center justify-center">
             <NavBar />
         </div>
-    <div className="w-screen h-screen bg-gradient-to-bl from-green-900 via-gray-900 to-black flex flex-col items-center justify-center ">
-    <div className="bg-slate-50 bg-opacity-5 w-full max-w-2xl flex flex-col justify-center rounded-2xl gap-3">
-        <Flex
-      h="70vh"
-      align="center"
-      justify="center"
-      fontSize="20px"
-      fontFamily="poppins"
-      
-    >
-    
-      <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
-        <Button  onClick={() => [setDataEdit({}), onOpen()]}>
-          NOVA APOSTA
-        </Button>
+        <div className="w-screen h-screen bg-gradient-to-bl from-green-900 via-gray-900 to-black flex flex-col items-center justify-center ">
+        <Resume income={income} expense={expense} total={total} />
+        <div className="bg-slate-50 bg-opacity-5 w-full max-w-2xl flex flex-col justify-center rounded-2xl gap-3">
 
-        <Box overflowY="auto" height="100%">
-          <Table mt="6">
-          <Thead>
-              <Tr>
-                <Th color={"whiteAlpha.900"} maxW = {100} fontSize="20px">
-                  Valor
-                </Th>
-                <Th color={"whiteAlpha.900"} maxW = {100} fontSize="20px">
-                  Times
-                </Th>
-                <Th p={0}></Th>
-                <Th p={0}></Th>
-              </Tr>
-            </Thead> 
-            <Tbody>
-            {data.filter((item:any)=>item.valor.toLowerCase().includes(busca.toLowerCase()) || item.times.toLowerCase().includes(busca.toLowerCase())).map(({ valor, times }, index) => (
-                <Tr key={index} cursor="pointer " _hover={{ bg: "gray.100" }}>
-                    <Td color={"whiteAlpha.900"} maxW={100}>{valor}</Td>
-                    <Td color={"whiteAlpha.900"} maxW={100}>{times}</Td>
-                  <Td p={0}>
-                    <EditIcon
-                      color={"whiteAlpha.900"}                    
-                      fontSize={20}
-                      onClick={() => [
-                        setDataEdit({ valor, times, index }),
-                        onOpen(),
-                      ]}
-                    />
-                  </Td>
-                  <Td p={0}>
-                    <DeleteIcon
-                      color={"whiteAlpha.900"}
-                      fontSize={20}
-                      onClick={() => handleRemove(times)}
-                    />
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </Box>
-      </Box>
-      {isOpen && (
-        <ModalComp
-          isOpen={isOpen}
-          onClose={onClose}
-          data={data}
-          setData={setData}
-          dataEdit={dataEdit}
-          setDataEdit={setDataEdit}
-        />
-      )}</Flex><input
-      type='text'
-      placeholder="Buscar..."
-      value={busca}
-      onChange={(ev) => setBusca(ev.target.value)}
-    ></input></div></div> 
-      </ChakraProvider>
+          <Flex
+            h="70vh"
+            align="center"
+            justify="center"
+            fontSize="20px"
+            fontFamily="poppins"
+
+
+          >
+
+            <Box maxW={800} w="100%" h="80vh" py={10} px={2} >
+              <Button onClick={() => [setDataEdit({}), onOpen()]}>
+                NOVA APOSTA
+              </Button>
+
+              <Box overflowY="auto" height="100%">
+                <Table mt="6">
+                  <Thead>
+                    <Tr>
+                      <Th color={"whiteAlpha.900"} maxW={100} fontSize="20px">
+                        Valor
+                      </Th>
+                      <Th color={"whiteAlpha.900"} maxW={100} fontSize="20px">
+                        Times
+                      </Th>
+                      <Th p={0}></Th>
+                      <Th p={0}></Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {data.filter((item: any) => item.valor.toLowerCase().includes(busca.toLowerCase()) || item.times.toLowerCase().includes(busca.toLowerCase())).map(({ valor, times }, index) => (
+                      <Tr key={index} cursor="pointer " _hover={{ bg: "gray" }}>
+                        <Td color={"whiteAlpha.900"} maxW={100}>{valor}</Td>
+                        <Td color={"whiteAlpha.900"} maxW={100}>{times}</Td>
+                        <Td p={0}>
+                          <EditIcon
+                            color={"whiteAlpha.900"}
+                            fontSize={20}
+                            onClick={() => [
+                              setDataEdit({ valor, times, index }),
+                              onOpen(),
+                            ]}
+                          />
+                        </Td>
+                        <Td p={0}>
+                          <DeleteIcon
+                            color={"whiteAlpha.900"}
+                            fontSize={20}
+                            onClick={() => handleRemove(times)}
+                          />
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </Box>
+            </Box>
+            {isOpen && (
+              <ModalComp
+                isOpen={isOpen}
+                onClose={onClose}
+                data={data}
+                setData={setData}
+                dataEdit={dataEdit}
+                setDataEdit={setDataEdit}
+              />
+            )}</Flex><input
+              type='text'
+              placeholder="Buscar..."
+              value={busca}
+              onChange={(ev) => setBusca(ev.target.value)}
+            ></input></div></div>
+    </ChakraProvider>
   );
 };
-       
+
+
+
+
+
