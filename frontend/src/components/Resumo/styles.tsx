8dbcd7cc232b8676002601e4e7ffd9c158649d48
Lyrton Marcell <lymarcell@gmail.com>
2022-12-12 16:39:52 -0300

    add:estilização dos componetes base editados

---
+++ frontend/src/components/Resumo/styles.tsx
@@ -0,0 +1,12 @@
+//npm i styled-components
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1120px;
+  width: 98%;
+  margin: 0 auto;
+  display: flex;
+  gap: 20px;
+  margin-top: -50px;
+  justify-content: space-around;
+`;
