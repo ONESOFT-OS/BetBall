b70e99d7a4106552fc06aa814aa70e88d6021841
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2022-12-23 11:00:55 -0300

    ajustes

--- frontend/src/components/SelectorHome.tsx
+++
@@ -1,8 +0,0 @@
-export function SelectorHome () {
-    return (
-        <div  className='flex flex-row items-center justify-center w-[424px] h-[45px] top-[610px] left-[536px]'>
-            <h1>Ranking</h1>
-            <h1>Jogos</h1>
-        </div>
-    )
-}
--- frontend/src/hooks/useAuth.tsx
+++
@@ -1,6 +0,0 @@
-import {AuthContext} from "../contexts/Auth/AuthContext";
-import {useContext} from "react";
-
-export const useAuth = () => {
-    return useContext(AuthContext);
-}
